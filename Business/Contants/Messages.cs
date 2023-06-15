b4b96cad46ab7a281f5f2118bd88e0c08535c6ce
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2023-06-15 17:15:18 +0300

    Magic String'den kurtulma.

---
+++ Business/Contants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün başarıyla Eklendi.";
+        public static string ProductDeleted = "Ürün başarıyla Silindi.";
+        public static string ProductUpdated = "Ürün başarıyla Güncellendi.";
+    }
+}
