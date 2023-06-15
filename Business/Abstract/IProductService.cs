1e5e9962db246f33994aaef5583b1128e3492f81
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2023-06-15 15:00:51 +0300

    İş katmanı, servis methodları tanımlandı

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        Product GetById(int productId);
+        List<Product> GetByCategoryId(int categoryId);
+        List<Product> GetList();
+        void Delete(Product product);
+        void Add(Product product);
+        void Update(Product product);
+    }
+}
