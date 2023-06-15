7b0dbfbb320217649e660675663ffbbe9bbea82d
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2023-06-15 14:21:03 +0300

    genericrepo ve genel metholar verildi

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,11 @@ namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T : class,IEntity, new()
     {
+        T Get(Expression<Func<T, bool>> filter);
+        IList<T> GetList(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
     }
 }
