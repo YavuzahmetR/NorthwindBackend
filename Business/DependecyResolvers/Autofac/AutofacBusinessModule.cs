d0d79a7398edfb8052a2cb7df0d226f0bb282328
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2023-06-19 19:49:29 +0300

    kullanıcılar için builder service yazıldı

--- Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
@@ -20,6 +20,9 @@ namespace Business.DependecyResolvers.Autofac
 
             builder.RegisterType<CategoryManager>().As<ICategoryService>();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
         }
     }
 }
