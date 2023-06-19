c59dc69554f40b766995f1975f8773ce2ff45a75
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2023-06-19 19:58:11 +0300

    token oluşturma ve erişim mthodu yazıldı

---
+++ Core/Utilities/Security/Jwt/ITokenHelper.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entites;
+using Core.Entities.Concrete;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
