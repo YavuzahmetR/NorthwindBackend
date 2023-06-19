d10d4ef49c911906c293771fe8272279653451d4
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2023-06-19 19:45:45 +0300

    Category ve Authentication sınıflarından veri tabanına bağlantı oluşturuldu

--- DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,8 +16,14 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;integrated security=true");
         }
 
-        DbSet<Product> Products {  get; set; }  
+        public DbSet<Product> Products {  get; set; }
 
-        DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
     }
 }
