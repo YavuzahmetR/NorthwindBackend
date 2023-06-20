dec17e1beba6b0eff5d49fd06db8905cb1faa845
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-06-21 00:58:08 +0300

    authmanager için gerekli mesajlar yazıldı.

--- Business/Contants/Messages.cs
+++ Business/Contants/Messages.cs
@@ -9,10 +9,22 @@ namespace Business.Contants
 {
     public static class Messages
     {
-        public static string ProductAdded = "Ürün başarıyla Eklendi.";
-        public static string ProductDeleted = "Ürün başarıyla Silindi.";
-        public static string ProductUpdated = "Ürün başarıyla Güncellendi.";
+        public static string ProductAdded = "Ürün başarıyla Eklendi";
 
-        public static string UserNotFound = "Kullanıcı Bulunamadı.";
+        public static string ProductDeleted = "Ürün başarıyla Silindi";
+
+        public static string ProductUpdated = "Ürün başarıyla Güncellendi";
+
+        public static string UserNotFound = "Kullanıcı Bulunamadı";
+
+        public static string PasswordNotFound = "Şifre Bulunamadı";
+
+        public static string SuccessfulLogin = "Giriş Başarılı";
+
+        public static string UserAlreadyExists = "Böyle Bir Kullanıcı Zaten Mevcut";
+
+        public static string UserRegistered = "Kullanıcı Başarıyla Kaydedildi";
+
+        public static string AccessCreateToken = "Access Token Başarıyla Oluşturuldu.";
     }
 }
