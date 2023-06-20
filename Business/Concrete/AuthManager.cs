6c0f9f0ce574957b3fbcdcdc0b4b8ba4d9b3ee5f
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-06-21 01:02:07 +0300

    dosya isim revizesi

--- Business/Concrete/AuthManager.cs
+++ Business/Concrete/AuthManager.cs
@@ -1,5 +1,5 @@
 ﻿using Business.Abstract;
-using Business.Contants;
+using Business.Constants;
 using Core.Entities.Concrete;
 using Core.Utilities.Result;
 using Core.Utilities.Security.Hashing;
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Business.Contants
+namespace Business.Constants
 {
     public static class Messages
     {
