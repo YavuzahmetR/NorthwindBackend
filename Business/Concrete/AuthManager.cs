89c9d94d0e028c353ec59ee644ae3f6bb79a47e2
Ahmet Yavuz Işık <ayavuzisik@ogr.trakya.edu.tr>
2023-06-20 21:51:40 +0300

    gerekli constructerlar yazıldı, authservice implementasyonu yapıldı

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using Core.Utilities.Result;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private IUserService _userService;
+        private ITokenHelper _tokenHelper;
+
+        public AuthManager(IUserService userService, ITokenHelper tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult UserExists(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
